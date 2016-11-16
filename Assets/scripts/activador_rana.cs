1212956e70df8fd1febd1a371e509fcc41e671c7
María Rubia Esteban <mariarubite7@gmail.com>
2016-11-16 18:05:35 +0100

    copia de proyecto original

---
+++ Assets/scripts/activador_rana.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class activador_rana : MonoBehaviour {
+	Animator animacion;
+	// Use this for initialization
+	void Start () {
+		animacion = GetComponent<Animator> ();
+	}
+
+	void OnTriggerEnter2D(Collider2D objeto){
+		if (objeto.tag == "Player") {
+			animacion.SetTrigger ("Salto");
+		}
+	}
+}
---
+++ Assets/scripts/checkpoint.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class checkpoint : MonoBehaviour {
+	private gamecontrol_script gcs;
+	public Sprite banderaActivada;
+	private bool activada = false;
+
+	// Use this for initialization
+	void Start () {
+		gcs = GameObject.Find ("GameControl").GetComponent<gamecontrol_script> ();
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter2D(Collider2D objeto) {
+		if (objeto.tag == "Player" && !activada) {
+			GetComponent<SpriteRenderer> ().sprite = banderaActivada;
+			gcs.checkpoint (transform.position);
+			activada = true;
+		}
+	}
+}
