74c42605bcdca89ff352d5e1c262a222d2ba60b6
María Rubia Esteban <mariarubite7@gmail.com>
2016-11-23 19:09:27 +0100

    Revert "Bloque de monedas aleatorias"
    This reverts commit 1018feb50fe222f45a6ca103acbd6ff494318a5f.

--- Assets/scripts/moneda.cs
+++ Assets/scripts/moneda.cs
@@ -2,19 +2,18 @@
 using System.Collections;
 
 public class moneda : MonoBehaviour {
-
-	private Rigidbody2D rb;
-
-	void Start (){
-		rb = GetComponent<Rigidbody2D> ();
-		rb.AddForce (new Vector2 (Random.Range (-200, 200), Random.Range (50, 100)));
+	// función para controlar cuando un objeto entra en el trigger
+	void OnTriggerEnter2D(Collider2D col){
+		Debug.Log ("Alguien me ha tocado");
+		Destroy (gameObject);
 	}
-	
 
-	void OnCollisionEnter2D(Collision2D col) {
-		if (col.gameObject.tag == "Player") {
-			Destroy (gameObject);
-		}
+	void OnTriggerExit2D(Collider2D col){
+		Debug.Log ("Han dejado de tocarme");
+	}
+
+	void OnTriggerStay2D(Collider2D col){
+		Debug.Log ("Me están tocando, me siento incómodo");
 	}
 }
 
