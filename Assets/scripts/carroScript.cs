06ce1293925ac292c610c677ee40fd33accccfcf
María Rubia Esteban <mariarubite7@gmail.com>
2016-12-07 18:27:11 +0100

    Ejercicios de prueba

---
+++ Assets/scripts/carroScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class carroScript : MonoBehaviour { //Sin terminar
+	public float velocidad = 10;
+	private Rigidbody2D rb;
+	public bool derecha = false;
+
+	void Start () {
+	
+	}
+
+	void Update () {
+		if (derecha) {
+			rb.velocity = new Vector2 (velocidad, 0);
+		} else {
+			
+		}
+	}
+}
---
+++ Assets/scripts/sensorScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sensorScript : MonoBehaviour {
+	public carroScript cs;
+
+	void OnTriggerEnter2D(Collider2D col){
+		cs.derecha = !cs.derecha;
+	}
+}
